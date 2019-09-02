3ed1030d9731a04989d77f4b5e9676d0feb2ae3f
Guilhermesav <54535484+Guilhermesav@users.noreply.github.com>
2019-09-02 08:55:33 -0300

    Update projeto_bloco.py

--- projeto_bloco.py
+++ projeto_bloco.py
@@ -1,23 +1,38 @@
 import pygame, random
-from tabulate import tabulate
 branco  = (255,255,255)
 preto = (0,0,0)
+verde = (0,255,0)
+azul = (0,0,156)
+red = (255,0,0)
+darkBlue = (2 , 24 , 89)
+cores = [verde,azul,red,darkBlue]
+
 pygame.mixer.init()
 
 pygame.font.init()
 
 largura_tela = 800
-
 altura_tela = 600
 
 tela = pygame.display.set_mode((largura_tela,altura_tela))
 
 terminou = False
 
-def mostra_titulo():
+class Aba():
+    def __init__(self,aba_type,cor):
+        self.largura = 200
+        self.altura = 60
+        self.x = 200 * aba_type
+        self.y = 0
+        self.area = pygame.Rect(self.x,self.y,self.largura,self.altura)
+        self.cor = cores.pop()
+    def desenha(self, tela):
+        pygame.draw.rect(tela,self.cor,self.area)
+
+def mostra_titulo(texto,x,y):
     font = pygame.font.Font(None,24)
-    text = font.render("S1",1,preto)
-    textpos = text.get_rect(centerx=tela.get_width()/2)
+    text = font.render(texto,1,preto)
+    textpos = text.get_rect(center=(x,y) )
     tela.blit(text,textpos)
     
 def mostrar_dados(texto,y):
@@ -32,13 +47,22 @@ def montar_tabela(texto,y):
     textpos = text.get_rect(center =(tela.get_width()/2 ,y))
     tela.blit(text,textpos)
 
+def mostra_titulo_aba(texto,x):
+    font = pygame.font.Font(None,20)
+    text = font.render(texto,1,branco)
+    textpos = text.get_rect(center =(x,30))
+    tela.blit(text, textpos)
+tela.fill(branco)   
+for i in range(0,4):
+    aba = Aba(i,cores)
+    aba.desenha(tela)
+        
 while not terminou:
 
     dicionario = {'alexandre': 456123789, 'anderson': 1245698456,
               'antonio': 123456456, 'carlos': 91257581,
               'cesar':987458, 'rosemary': 789456125 }
-    
-    
+
     valores_Bytes = dicionario.values()
 
     soma = sum(dicionario.values())
@@ -67,22 +91,24 @@ while not terminou:
     print("Media de memoria usada:", round(soma_media,2), "Mb")
     
 
+
+    mostra_titulo("Aba 1",100,25)
+    #montar_tabela("ACME Inc.           Uso do espaço em disco pelos usuários",50)
+    #mostrar_dados("", 30+soma_indices*10)
     
-    tela.fill(branco)
-    mostra_titulo()
-    montar_tabela("ACME Inc.           Uso do espaço em disco pelos usuários",50)
-    montar_tabela("Nr.    Usuário        Espaço utilizado     % do uso", 100)
-    mostrar_dados(dicionario, 30+soma_indices*10)
-   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminou = True
         if event.type == pygame.MOUSEBUTTONDOWN and event.button ==1:
             pos = pygame.mouse.get_pos()
-            if aba1.area.collidepoint(pos):
-                
+            if aba.area.collidepoint(pos):
+                terminou = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminou = True
     
     pygame.display.update()
     
 pygame.display.quit()
      
+
