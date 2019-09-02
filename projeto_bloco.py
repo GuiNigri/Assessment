3cbc19dba0542ccda8bab344fd4bb8398b8bcb5a
Guilhermesav <54535484+Guilhermesav@users.noreply.github.com>
2019-09-02 09:35:00 -0300

    Update projeto_bloco.py

--- projeto_bloco.py
+++ projeto_bloco.py
@@ -5,7 +5,7 @@ verde = (0,255,0)
 azul = (0,0,156)
 red = (255,0,0)
 darkBlue = (2 , 24 , 89)
-cores = [verde,azul,red,darkBlue,preto]
+cores = [azul,red,darkBlue,preto]
 
 pygame.mixer.init()
 
@@ -25,13 +25,13 @@ class Aba():
         self.x = 200 * aba_type
         self.y = 0
         self.area = pygame.Rect(self.x,self.y,self.largura,self.altura)
-        self.cor = cores.pop()
+        self.cor = cores[aba_type]
     def desenha(self, tela):
         pygame.draw.rect(tela,self.cor,self.area)
 
 def mostra_titulo(texto,x,y):
     font = pygame.font.Font(None,24)
-    text = font.render(texto,1,branco)
+    text = font.render(texto,1,preto)
     textpos = text.get_rect(center=(x,y) )
     tela.blit(text,textpos)
     
@@ -63,7 +63,7 @@ def cria_abas():
 
 
 tela.fill(branco)
-aba1,aba2,aba3,aba4 =  cria_abas()
+aba0,aba1,aba2,aba3 =  cria_abas()
 while not terminou:
 
     dicionario = {'alexandre': 456123789, 'anderson': 1245698456,
@@ -108,9 +108,23 @@ while not terminou:
             terminou = True
         if event.type == pygame.MOUSEBUTTONDOWN and event.button ==1:
             pos = pygame.mouse.get_pos()
-            if aba.area.collidepoint(pos):
+            if aba0.area.collidepoint(pos):
                 tela.fill(branco)
                 aba0, aba1, aba2, aba3 = cria_abas()
+                mostra_titulo("ACME Inc.           Uso do espaço em disco pelos usuários",400,150)
+                montar_tabela("------------------------------------------------------------------------",175)
+                mostrar_dados("1  alexandre, 456123789",150+soma_indices*10)
+                mostrar_dados("2  anderson 1245698456",170+soma_indices*10)
+            if aba1.area.collidepoint(pos):
+                tela.fill(branco)
+                aba0, aba1, aba2, aba3 = cria_abas()
+            if aba2.area.collidepoint(pos):
+                tela.fill(branco)
+                aba0, aba1, aba2, aba3 = cria_abas()
+            if aba3.area.collidepoint(pos):
+                tela.fill(branco)
+                aba0, aba1, aba2, aba3 = cria_abas()
+                
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminou = True
