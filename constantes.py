8bfa1d707a05b4e8b8ab104371b6d5eedc5208cc
Guilherme Saraiva <49844284+guilhermes14@users.noreply.github.com>
2019-11-25 08:54:46 -0300

    Add files via upload

---
+++ constantes.py
@@ -0,0 +1,9 @@
+branco  = (255,255,255)
+preto = (0,0,0)
+verde = (0,255,0)
+azul = (0,0,156)
+red = (255,0,0)
+darkBlue = (2 , 24 , 89)
+cores = [azul,red,darkBlue,preto]
+
+largura_tela, altura_tela = 800, 1024
